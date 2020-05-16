2b18149fddd2950bac581df2527a2af77621fc85
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-16 16:20:08 -0700

    add grid verticals and horizontals

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 5;
 const width = 600;
 const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
-	element: document.body, // where to show the render
-	engine: engine, // specify which engine to use
-	options: {
-		width,
-		height
-	}
+  element: document.body, // where to show the render
+  engine: engine, // specify which engine to use
+  options: {
+    width,
+    height,
+  },
 });
 
 Render.run(render);
@@ -19,14 +20,26 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }), // top wall
-	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }), // bottom wall
-	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }), // left wall
-	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }) // right wall
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }), // top wall
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }), // bottom wall
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }), // left wall
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }), // right wall
 ];
 
 World.add(world, walls);
 
 // Maze generation
 
-const grid = Array(3).fill(null).map(() => Array(3).fill(false));
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+console.log(grid, verticals, horizontals);
