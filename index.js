d3a5d594fcb28f6a8c32fd4e56de9a50f62c4952
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-16 15:49:17 -0700

    add grid generator

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const width = 600;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
 	element: document.body, // where to show the render
 	engine: engine, // specify which engine to use
 	options: {
-		width: 800,
-		height: 600
+		width,
+		height
 	}
 });
 
@@ -16,12 +19,14 @@ Runner.run(Runner.create(), engine);
 
 // Walls
 const walls = [
-	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-	Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-	Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-	Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }), // top wall
+	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }), // bottom wall
+	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }), // left wall
+	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }) // right wall
 ];
 
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(200, 200, 50, 50));
+// Maze generation
+
+const grid = Array(3).fill(null).map(() => Array(3).fill(false));
