8dbee0465bbe149b7352667f16fe3a6fcbb1f923
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-20 15:37:04 -0700

    display success message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cellsHorizontal = 10;
-const cellsVertical = 7;
+const cellsVertical = 6;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
@@ -208,6 +208,7 @@ Events.on(engine, 'collisionStart', event => {
       labels.includes(collision.bodyA.label) && 
       labels.includes(collision.bodyB.label)
     ) {
+      document.querySelector('.winner').classList.remove('hidden');
       world.gravity.y = 1;
       world.bodies.forEach(body => {
         if (body.label === 'wall') {
